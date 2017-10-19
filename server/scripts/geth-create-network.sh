2c2b5349bd0c26e5bfa3feab7bca6cb9d7ea0047
Chris Smith <me@chrisryansmith.com>
2017-10-19 15:52:09 +0400

    pull network id from json file and mkdir /server/nodes if it doesn't already exist

--- server/scripts/geth-create-network.sh
+++ server/scripts/geth-create-network.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
 NETWORK_NAME=$1
+
 echo $NETWORK_NAME
 
 # if ! [ -x "$(command -v bootnode)" ]; then
@@ -8,6 +9,14 @@ echo $NETWORK_NAME
   # TODO: Fix for Chris because bootnode not working
   # alias bootnode=/Users/chrissmith/go-ethereum/build/bin/bootnode
 # fi
+
+# Packages Needed:
+## screen
+## jq
+
+NETWORK_ID=$(jq -r '.config | .chainId' ./server/scripts/${NETWORK_NAME}.json)
+
+mkdir -p ./server/nodes/
 
 bootnode -genkey ./server/nodes/${NETWORK_NAME}.key -writeaddress
 bootnode -nodekey ./server/nodes/${NETWORK_NAME}.key -writeaddress > ./server/nodes/enode
@@ -19,18 +28,18 @@ echo "Started bootnode"
 echo "Starting Node 1"
 rm -rf ./server/nodes/node1
 geth --datadir ./server/nodes/node1 init ./server/scripts/${NETWORK_NAME}.json
-screen -dmS 'Node1' geth --datadir ./server/nodes/node1 --networkid 40851 --port 30303 --bootnodes enode://$(cat ./server/nodes/enode)@127.0.0.1:30301
+screen -dmS 'Node1' geth --datadir ./server/nodes/node1 --networkid $NETWORK_ID --port 30303 --bootnodes enode://$(cat ./server/nodes/enode)@127.0.0.1:30301
 echo "Started Node 1"
 
 echo "Starting Node 2"
 rm -rf ./server/nodes/node2
 geth --datadir ./server/nodes/node2 init ./server/scripts/${NETWORK_NAME}.json
-screen -dmS 'Node2' geth --datadir ./server/nodes/node2 --networkid 40851 --port 30304 --bootnodes enode://$(cat ./server/nodes/enode)@127.0.0.1:30301
+screen -dmS 'Node2' geth --datadir ./server/nodes/node2 --networkid $NETWORK_ID --port 30304 --bootnodes enode://$(cat ./server/nodes/enode)@127.0.0.1:30301
 echo "Started Node 2"
 
 echo "Starting Node 3"
 rm -rf ./server/nodes/node3
 geth --datadir ./server/nodes/node3 init ./server/scripts/${NETWORK_NAME}.json
-screen -dmS 'Node3' geth --datadir ./server/nodes/node3 --networkid 40851 --port 30305 --bootnodes enode://$(cat ./server/nodes/enode)@127.0.0.1:30301
+screen -dmS 'Node3' geth --datadir ./server/nodes/node3 --networkid $NETWORK_ID --port 30305 --bootnodes enode://$(cat ./server/nodes/enode)@127.0.0.1:30301
 echo "Started Node 3"
 
