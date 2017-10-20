a761f83a2eb19173da0129b74646d9710461b258
Madhan Ramalingam <madhancr@gmail.com>
2017-10-20 11:39:13 +0400

    integrate network states

--- server/utils.js
+++ server/utils.js
@@ -1,6 +1,7 @@
 var fs = require('fs');
 
 const exec = require('child_process').exec;
+const execSync = require('child_process').execSync;
 
 function exec_script(){
     exec('/Users/madhan/Courses/ConsenSys/DubaiHackathon/private-network-manager/test.sh',
@@ -16,7 +17,19 @@ function exec_script(){
 
 
 function get_state(){
-      return fs.readFileSync("networks.txt");
+
+    var network_states = fs.readFileSync("networks.txt");
+    network_states = JSON.parse(network_states)
+    console.log(network_states)
+    console.log(execSync(`pwd`))
+    for (let i = 0; i <  network_states.length; i++) {
+        let state = network_states[i]
+        console.log(state)
+        status = execSync('./server/scripts/check-network.sh ' + state["networkId"]).toString()
+        state["status"] =  status
+    }
+    console.log(network_states)    
+    return network_states;
 }
 
 function save_state(){
