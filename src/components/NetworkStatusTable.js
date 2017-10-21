6192e37c1153e447209fe9fe214355c913c700fa
Carlo P. Las Marias <carlol@gmail.com>
2017-10-21 10:23:12 +0400

    tabs | monitor network state

--- src/components/NetworkStatusTable.js
+++ src/components/NetworkStatusTable.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react'
 import axios from 'axios'
-import { Table, Form, Button, Dropdown } from 'semantic-ui-react';
+import { Table, Form, Button, Dropdown, Message, Icon } from 'semantic-ui-react';
 
 import '../css/oswald.css'
 import '../css/open-sans.css'
@@ -15,37 +15,68 @@ class NetworkStatusTable extends Component {
 
     
     this.state = {
-      networks: {},
       statusMessage: '',
+      selectedNetwork: '',
+      action: '',
+      running: false,
     }
 
     this.handleChange = this.handleChange.bind(this);
     this.handleStartStop = this.handleStartStop.bind(this);
     this.handleEditNetworkDetails = this.handleEditNetworkDetails.bind(this);
+    this.resetState = this.resetState.bind(this);
+    this.checkNetworksStatus = this.checkNetworksStatus.bind(this);
   }
 
+  componentWillReceiveProps() {
+    this.checkNetworksStatus();
+  }
+  
+  checkNetworksStatus() {
+    const { networks } = this.props;
+    Object.keys(networks).map((network) => {
+      console.log(networks[network].status);
+      if (networks[network].status === 'running') {
+        this.setState({ running: true });
+      }
+    })
+  }
 
   handleChange(e, data) {
     this.setState({ [e.target.name]: e.target.value });
+  }
+
+  resetState() {
+    this.setState({ statusMessage: '', selectedNetwork: '', action: '', running: false }, this.checkNetworksStatus)
   }
 
   handleStartStop(params) {
     if (params === 'stop') {
       axios.post('/geth/network/stop')
         .then((statusMessage) => {
-          this.setState({ statusMessage: statusMessage.data.message });
+          console.log('**** STOP ');
+          this.setState({
+            statusMessage: statusMessage.data.message,
+            action: 'stop',
+            running: false,
+          });
           this.props.updateNetworksStatus();
+          setTimeout(this.resetState, 3000);
         })
         .catch(err => this.setState({ statusMessage: `Error: ${err}` }));
-    } else {
-      axios.post(`/geth/network/${params.networkId}/start`, params)
+      } else {
+        axios.post(`/geth/network/${params.networkId}/start`, params)
         .then((statusMessage) => {
-          this.setState({ statusMessage: statusMessage.data.message });
+          this.setState({ statusMessage: statusMessage.data.message, selectedNetwork: params.name, action: 'starting' });
           let i = 0;
           let interval = setInterval(() => {
             console.log('check');
             this.props.updateNetworksStatus();
-            if (i > 5 || this.props.networks[params.name].status === 'running') clearInterval(interval);
+            if (i > 5 || this.props.networks[params.name].status === 'running') {
+              clearInterval(interval);
+              this.setState({ action: 'start' })
+              setTimeout(this.resetState, 3000);
+            }
             i += 1;
           }, 1000);
         })
@@ -83,10 +114,26 @@ class NetworkStatusTable extends Component {
                 network={networks[name]}
                 handleStartStop={this.handleStartStop}
                 handleEditNetworkDetails={this.handleEditNetworkDetails}
+                running={this.state.running}
               />)}
           </Table.Body>
         </Table>
-        {this.state.statusMessage}
+        {this.state.statusMessage !== '' && (
+          <Message
+            positive={this.state.action === 'start'}
+            warning={this.state.action === 'starting'}
+            icon={this.state.action === 'starting'}
+            negative={this.state.action === 'stop'}
+          >
+            {this.state.action === 'starting' && (
+              <Icon name='circle notched' loading />
+            )}  
+            <Message.Content>
+              <Message.Header>Network: {this.state.selectedNetwork}</Message.Header>
+              <p>Network {this.state.action === 'stop' ? 'stopped' : this.state.action === 'start' ? 'started' : 'starting'}</p>
+            </Message.Content>
+          </Message>
+        )}
 
       </div>
     );
